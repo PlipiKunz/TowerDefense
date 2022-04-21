82880a2be7a98203fc4fd6ceaaf0ab2145a4aaff
PlipiKunz <johnathankunz@gmail.com>
2022-04-21 00:32:30 -0600

    bullet changes

---
+++ TowerDefense/TowerDefense/Components/Bomb.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using Systems;
+
+namespace Components
+{
+    class Bomb : Component
+    {
+        public int range;
+        public TargetType targetType;
+
+        public Bomb(int range, TargetType targetType)
+        {
+            this.range = range;
+            this.targetType = targetType;
+        }
+    }
+}
